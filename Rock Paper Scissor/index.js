d621aace48a9a90064f624e66fc60d7ae9aced6d
Vishwas Rawat <123429318+Vishwas-Rawat@users.noreply.github.com>
2024-03-12 14:17:57 +0530

    Create index.js

---
+++ Rock Paper Scissor/index.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Madimi+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        Rock Paper Scissor
+        <img src="bg.png" alt="rock paper scissor game" height="180px">
+    </div>
+    <div class="container">
+        <span class="cp">Comp points</span>
+        <div class="box ran"></div>
+        <div class="box myturn"></div>
+        <span class="up">Your points</span>
+    </div>
+
+    <div class="btn">
+    <button class="keys rock"></button>
+    <button class="keys paper"></button>
+    <button class="keys scissor"></button>
+    </div>
+
+    <h2>Select your move 👆</h2>
+
+    <div class="reset">
+        <input class="reload" type="reset" name="" id="">
+    </div>
+
+    <script src="index.js"></script>
+</body>
+</html>
